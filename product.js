69dc64d01fcc23ec95ae93c833a6efd19b8d5373
dayan003 <dayan97876@gmail.com>
2024-05-27 15:20:07 +0530

    added js

---
+++ product.js
@@ -0,0 +1,22 @@
+//Product Search Functionalit
+
+var productContainer = document.getElementById("productcontainer")
+var search = document.getElementById("search")
+var productlist = productContainer.querySelectorAll("div")
+
+search.addEventListener("keyup", function (){
+    var enteredValue = event.target.value.toUpperCase()
+
+    for (count = 0; count < productlist.length; count = count + 1) 
+    {
+        var productname = productlist[count].querySelector("hl").textContent
+
+        if (productname.toUpperCase().indexOf(enteredValue) < 0) 
+        {
+            productlist[count].style.display = "none"
+        }
+        else {
+            productlist[count].style.display = "block"
+        }
+    }
+})
